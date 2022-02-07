76ef98a8dade162af87156bddfa9de91a01bc1fc
Pranav Satish Kumar <98470657+TheMegaCoderPro@users.noreply.github.com>
2022-02-07 18:29:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function add() {
+     var result =
+     Number(document.getElementById("firstno").value) +
+     Number(document.getElementById("secondno").value);
+     document.getElementById("result").innerHTML=result;
+     }
+     function sub() {
+     var result =
+     Number(document.getElementById("firstno").value) -
+     Number(document.getElementById("secondno").value);
+     document.getElementById("result").innerHTML=result;
+     }
+     function multi() {
+     var result =
+     Number(document.getElementById("firstno").value) *
+     Number(document.getElementById("secondno").value);
+     document.getElementById("result").innerHTML=result;
+     }
+     function div() {
+     var result =
+     Number(document.getElementById("firstno").value) /
+     Number(document.getElementById("secondno").value);
+     document.getElementById("result").innerHTML=result;
+     }
+     
